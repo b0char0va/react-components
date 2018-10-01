9f70bfad729740fa1219922ad277b898dc887f98
Mona Bocharova <bocharova.mona@gmail.com>
2018-10-01 14:34:53 -0700

    finished React Component

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
 // TODO
+// var Cucumbers = (props) => (<li>{props.item}</li>);
+//
+// var Kale = (props) => (<li>{props.item}</li>);
+
+
+class GroceryListItem extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            done: false
+        };
+    }
+
+    onListItemHover() {
+        (this.setState)({
+            done: !this.state.done
+        });
+    }
+
+    render(){
+        var style = {
+            fontWeight: this.state.done ? 'bold' : 'normal'
+        };
+        return (
+            <li style={style} onMouseOver={this.onListItemHover.bind(this)}>{this.props.item}</li>
+        );
+    }
+}
+
+var GroceryList = (props) => (
+    <ul>
+        {props.items.map(item =>
+            <GroceryListItem item={item} />
+        )}
+    </ul>
+);
+
+var App = () => (
+    <div>
+        <h2>My Grocery List</h2>
+        <GroceryList items={['Cucumbers', 'Kale']}/>
+    </div>
+);
+
+
+
+ReactDOM.render(<App/>, document.getElementById("app"));
